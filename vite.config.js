150c1b03fc01c13b576196f6b7d3954e5f61bd4e
Marcelo Giordano <marceloguterres@IMaggoBook-Air.local>
2026-04-15 14:08:26 -0300

    fix: transferir nexus para pasta public para ignorar engine vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     minify: 'esbuild',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nexus: resolve(__dirname, 'nexus.html')
-      }
-    }
   },
   server: {
     port: 3000,
